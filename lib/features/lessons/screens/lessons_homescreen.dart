0a16cedb354f2d66f8b76f242656d9afe98f999c
Ashwani Kumar Singh <ashwanikumarsingh.varanasi@gmail.com>
2023-09-18 16:54:41 +0530

    staggeredGrid

--- lib/features/lessons/screens/lessons_homescreen.dart
+++ lib/features/lessons/screens/lessons_homescreen.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:samskritam/common/widgets/custom_appbars.dart';
 import 'package:samskritam/common/widgets/loading_screen.dart';
 import 'package:samskritam/common/widgets/profile_pic.dart';
 import 'package:samskritam/features/lessons/controller/lesson_controller.dart';
+import 'package:samskritam/models/lesson_info.dart';
 
 class LessonsHomeScreen extends ConsumerStatefulWidget {
   const LessonsHomeScreen({Key? key}) : super(key: key);
@@ -43,6 +45,7 @@ class _LessonsHomeScreenState extends ConsumerState<LessonsHomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: CustomBlueAppbar(
         onLeadingPressed: () {},
@@ -54,25 +57,118 @@ class _LessonsHomeScreenState extends ConsumerState<LessonsHomeScreen> {
       body: Column(
         children: [
           CustomLowerAppBar(),
-          Center(
-            child: ElevatedButton(
-              child: Text("Click me"),
-              onPressed: () {
-                // tryToAddDataToFirestore();
-                tryReadingMapData();
+          Expanded(
+            child: StreamBuilder(
+              stream: ref.read(lessonControllerProvider).getLessons(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const LoadingScreen();
+                }
+
+                // return Text(snapshot.data![index].imageUrl.toString());
+                return ScrollConfiguration(
+                  behavior: ScrollConfiguration.of(context)
+                      .copyWith(scrollbars: false),
+                  child: SingleChildScrollView(
+                    child: Center(
+                      child: SizedBox(
+                        width: width * 0.75,
+                        child: StaggeredGrid.count(
+                          axisDirection: AxisDirection.down,
+                          crossAxisCount: 6,
+                          mainAxisSpacing: 4,
+                          crossAxisSpacing: 4,
+                          children:
+                              lessonTile(snapshot.data, [], context),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
               },
             ),
-          ),
-          StreamBuilder(
-            stream: ref.read(lessonControllerProvider).getLessons(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const LoadingScreen();
-              }
-              return Text("chekcing" + snapshot.data![0].imageUrl.toString());
-            },
           )
         ],
+      ),
+    );
+  }
+}
+
+List<Widget> lessonTile(List<LessonInfo>? lessons, List? completedLessons,
+    BuildContext context) {
+  List<Widget> ite = [];
+
+  for (int i = 0; i < lessons!.length; i++) {
+    int s = lessons[i].cellCount!;
+
+    Widget w;
+    // if (completedLessons!.contains(i)) {
+    //   w = StaggeredGridTile.count(
+    //     crossAxisCellCount: s,
+    //     mainAxisCellCount: 2,
+    //     child: Container(
+    //       padding: const EdgeInsets.all(8),
+    //       child: lessonImage(lessons[i], () => null, context, true),
+    //     ),
+    //   );
+    // } else {
+      w = StaggeredGridTile.count(
+        crossAxisCellCount: s,
+        mainAxisCellCount: 2,
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: lessonImage(lessons[i], () => null, context, false),
+        ),
+      );
+    // }
+    ite.add(w);
+  }
+
+  // ite.add(w);
+
+  return ite;
+}
+
+Widget lessonImage(LessonInfo? lessonData, Function() onPressed,
+    BuildContext context, bool isCompletedLesson) {
+  if (lessonData == null) {
+    return const SizedBox();
+  }
+
+  if (lessonData.type == "image") {
+    return SizedBox(
+      width: 200,
+      height: 200,
+      child: Image.network(
+        lessonData.imageUrl!,
+      ),
+    );
+  } else {
+    return InkWell(
+      onTap: () {
+        // Navigator.pushNamedAndRemoveUntil(
+        //     context, "/learnlesson", (route) => false,
+        //     arguments: {"data": lessonData["lessons"]});
+      },
+      child: Opacity(
+        opacity: isCompletedLesson ? .5 : 1,
+        child: AspectRatio(
+          aspectRatio: 1.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: ClipOval(
+                  child: Image.network(lessonData.imageUrl.toString()),
+                ),
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              Text(lessonData.name!),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/features/lessons/screens/lessons_screen.dart
+++ lib/features/lessons/screens/lessons_screen.dart
@@ -1,122 +1,122 @@
-import 'package:flutter/material.dart';
-import 'package:samskritam/common/config/apptheme/theme_colors.dart';
-import 'package:samskritam/common/utils/dialog/alert_dialog.dart';
-import 'package:samskritam/common/widgets/custom_button.dart';
-import 'package:samskritam/common/widgets/loading_screen.dart';
-import 'package:samskritam/features/lessons/screens/route_lesson.dart';
+// import 'package:flutter/material.dart';
+// import 'package:samskritam/common/config/apptheme/theme_colors.dart';
+// import 'package:samskritam/common/utils/dialog/alert_dialog.dart';
+// import 'package:samskritam/common/widgets/custom_button.dart';
+// import 'package:samskritam/common/widgets/loading_screen.dart';
+// import 'package:samskritam/features/lessons/screens/route_lesson.dart';
 
-class LearnLesson extends StatefulWidget {
-  const LearnLesson({super.key});
+// class LearnLesson extends StatefulWidget {
+//   const LearnLesson({super.key});
 
-  // final Map<dynamic,dynamic>? lessonData = setting[a];
+//   // final Map<dynamic,dynamic>? lessonData = setting[a];
 
-  @override
-  State<LearnLesson> createState() => _LearnLessonState();
-}
+//   @override
+//   State<LearnLesson> createState() => _LearnLessonState();
+// }
 
-class _LearnLessonState extends State<LearnLesson> {
-  double sliderValue = 3;
-  ScrollController? _lessonController;
+// class _LearnLessonState extends State<LearnLesson> {
+//   double sliderValue = 3;
+//   ScrollController? _lessonController;
 
-  @override
-  Widget build(BuildContext context) {
-    _lessonController = ScrollController();
-    double width = MediaQuery.of(context).size.width;
-    var args = (ModalRoute.of(context)?.settings.arguments ?? {}) as Map;
-    final Map<dynamic, dynamic>? lessonData = args["data"];
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-              child: Row(children: [
-                IconButton(
-                    onPressed: () {
-                      simpleCancelAlertDialog(context, "Exiting...",
-                          // "Do you want to quit?", () => goToHomePage(context));
-                          "Do you want to quit?", () {});
-                    },
-                    icon: const Icon(Icons.cancel)),
-                Expanded(
-                  child: sliderButton(
-                      value: sliderValue,
-                      divisions: lessonData?.length ?? 1,
-                      onChanged: (change) {},
-                      // onChanged: (change) {
-                      //   setState(() {
-                      //     sliderValue = change;
-                      //   });
-                      // },
-                      context: context),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon:  Icon(
-                    Icons.info_outline,
-                    color: customDullWhite,
-                  ),
-                ),
-                // const Text(
-                //   "8",
-                //   style: TextStyle(color: Colors.red),
-                // ),
-                const SizedBox(
-                  width: 8,
-                )
-              ]),
-            ),
-            Expanded(
-              child: SizedBox(
-                width: double.infinity,
-                // color: Colors.red,
-                child: ListView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
-                    controller: _lessonController,
-                    itemCount: (lessonData == null) ? 1 : lessonData.length - 1,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, i) {
-                      if (lessonData == null) {
-                        return SizedBox(
-                          width: width - 16,
-                          child: const LoadingScreen(),
-                        );
-                      }
+//   @override
+//   Widget build(BuildContext context) {
+//     _lessonController = ScrollController();
+//     double width = MediaQuery.of(context).size.width;
+//     var args = (ModalRoute.of(context)?.settings.arguments ?? {}) as Map;
+//     final Map<dynamic, dynamic>? lessonData = args["data"];
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Column(
+//           children: [
+//             SizedBox(
+//               height: 50,
+//               child: Row(children: [
+//                 IconButton(
+//                     onPressed: () {
+//                       simpleCancelAlertDialog(context, "Exiting...",
+//                           // "Do you want to quit?", () => goToHomePage(context));
+//                           "Do you want to quit?", () {});
+//                     },
+//                     icon: const Icon(Icons.cancel)),
+//                 Expanded(
+//                   child: sliderButton(
+//                       value: sliderValue,
+//                       divisions: lessonData?.length ?? 1,
+//                       onChanged: (change) {},
+//                       // onChanged: (change) {
+//                       //   setState(() {
+//                       //     sliderValue = change;
+//                       //   });
+//                       // },
+//                       context: context),
+//                 ),
+//                 IconButton(
+//                   onPressed: () {},
+//                   icon:  Icon(
+//                     Icons.info_outline,
+//                     color: customDullWhite,
+//                   ),
+//                 ),
+//                 // const Text(
+//                 //   "8",
+//                 //   style: TextStyle(color: Colors.red),
+//                 // ),
+//                 const SizedBox(
+//                   width: 8,
+//                 )
+//               ]),
+//             ),
+//             Expanded(
+//               child: SizedBox(
+//                 width: double.infinity,
+//                 // color: Colors.red,
+//                 child: ListView.builder(
+//                     physics: const NeverScrollableScrollPhysics(),
+//                     controller: _lessonController,
+//                     itemCount: (lessonData == null) ? 1 : lessonData.length - 1,
+//                     scrollDirection: Axis.horizontal,
+//                     itemBuilder: (context, i) {
+//                       if (lessonData == null) {
+//                         return SizedBox(
+//                           width: width - 16,
+//                           child: const LoadingScreen(),
+//                         );
+//                       }
 
-                      Map<dynamic, dynamic>? currLesson = lessonData[i + 1];
-                      return Container(
-                        width: width - 16,
-                        height: 40,
-                        // color: Colors.blue,
-                        margin: const EdgeInsets.all(8),
-                        child: RouteLesson(
-                          lessonData: currLesson,
-                          nextQuestion: () {
-                            _lessonController?.animateTo(
-                                _lessonController!.offset + width,
-                                duration: const Duration(milliseconds: 300),
-                                curve: Curves.decelerate);
-                            // print(_lessonController.toString());
-                            setState(() {
-                              sliderValue =
-                                  ((i + 1) / (lessonData.length - 2)) * 100;
-                            });
-                          },
-                        ),
-                        // child: Akshara(lessonData: currLesson, nextQuestion: () {
-                        //   _lessonController?.animateTo(_lessonController!.offset + width, duration: const Duration(milliseconds: 500), curve: Curves.decelerate);
-                        //   // print(_lessonController.toString());
-                        //   setState(() {
-                        //     sliderValue= ((i+1)/(lessonData.length - 2)) * 100;
-                        //   });
-                        // },),
-                      );
-                    }),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//                       Map<dynamic, dynamic>? currLesson = lessonData[i + 1];
+//                       return Container(
+//                         width: width - 16,
+//                         height: 40,
+//                         // color: Colors.blue,
+//                         margin: const EdgeInsets.all(8),
+//                         child: RouteLesson(
+//                           lessonData: currLesson,
+//                           nextQuestion: () {
+//                             _lessonController?.animateTo(
+//                                 _lessonController!.offset + width,
+//                                 duration: const Duration(milliseconds: 300),
+//                                 curve: Curves.decelerate);
+//                             // print(_lessonController.toString());
+//                             setState(() {
+//                               sliderValue =
+//                                   ((i + 1) / (lessonData.length - 2)) * 100;
+//                             });
+//                           },
+//                         ),
+//                         // child: Akshara(lessonData: currLesson, nextQuestion: () {
+//                         //   _lessonController?.animateTo(_lessonController!.offset + width, duration: const Duration(milliseconds: 500), curve: Curves.decelerate);
+//                         //   // print(_lessonController.toString());
+//                         //   setState(() {
+//                         //     sliderValue= ((i+1)/(lessonData.length - 2)) * 100;
+//                         //   });
+//                         // },),
+//                       );
+//                     }),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
